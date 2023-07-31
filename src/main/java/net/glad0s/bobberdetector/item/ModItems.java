6ad0343999064be15ab39e5348022dc0f112f2ac
laserman120 <lukash01@aon.at>
2023-07-31 18:50:19 +0200

    Removed unnecessary item

--- src/main/java/net/glad0s/bobberdetector/item/ModItems.java
+++ src/main/java/net/glad0s/bobberdetector/item/ModItems.java
@@ -11,10 +11,6 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, BobberDetector.MOD_ID);
-
-    public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
-
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
